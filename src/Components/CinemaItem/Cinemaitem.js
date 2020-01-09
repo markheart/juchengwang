a92b1b325b168109c3b2aa2cb8586032e533a258
YongLiSong <>
2020-01-09 14:38:00 +0800

    cinema

--- src/Components/CinemaItem/Cinemaitem.js
+++ src/Components/CinemaItem/Cinemaitem.js
@@ -15,7 +15,7 @@ class Cinemaitem extends Component {
             <span className={style.cinemaname}>{this.props.data.name}</span>
             <span>{this.props.data.count}场在售演出</span>
           </p>
-          <span className={style.morecont}><NavLink to="/" id={this.props.data.vid}>...</NavLink></span>
+          <span className={style.morecont}><NavLink to="/" id={this.props.data.id}>...</NavLink></span>
         </h2>
 
 
@@ -24,16 +24,26 @@ class Cinemaitem extends Component {
 
         <MySwiper homeSwiper={
                 {
-                  slidesPerView: 3,
-                  spaceBetween: 30,
+                  slidesPerView: 2.8,
                 }}>
           {
             this.props.data.showList.map(item=>
-              <div className="swiper-slide" key={item.id}>
-                <img src={item.pic} alt={item.schedular_name} className={style.swiperitem}/>
+              <div className={"swiper-slide"+" "+style.swiperbox} key={item.id}>
+                <p>{item.show_time}<span></span></p>
+                <img src={item.pic} alt={item.schedular_name} className={style.swiperimg}/>
               </div>
               )
           }
+
+          {/*---------------------------- 显示最后的那个查看更多 ----------------------------*/}
+          {
+            // swiper最后有个查看更多,当轮播个数大于8才会显示
+            this.props.data.showList.length>8 ? <div className={"swiper-slide"+" "+style.swiperbox}>
+            <p><span></span></p>
+            <div className={style.swiperlast}><NavLink to="/list">查看更多></NavLink></div>
+          </div> : null
+          }
+          
         </MySwiper>
       </div>
     )
